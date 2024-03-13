0eadfb4d40c92892a00e5ec54cf4978f8cbc0eeb
huntrreed <146265520+huntrreed@users.noreply.github.com>
2024-03-12 20:43:57 -0600

    added the JS TODOs

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,50 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: '/index.html', //path to html file
+        title: 'Text Editor',
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js', // source service worker file
+        swDest: 'service-worker.js', // filename for the  output 
+      }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Text Editor',
+        short_name: 'TxtEditor',
+        description: 'TextEditor challenge 19',
+        background_color: '#BCB88A',
+        theme_color: '#5C4033',
+        start_url: '/',
+        icons: [
+          {
+            src: path.resolve('./src.images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512], 
+            destination: path.join('icons')
+          },
+        ],
+      }),
     ],
+    
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
       ],
     },
   };
